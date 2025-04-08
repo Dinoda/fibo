331c3f7516b8f56afcec882e5207187f21beac36
Debian <debian@vps-fbb5e062.vps.ovh.net>
2025-04-08 08:53:14 +0000

    Added basic resolvers for the server package

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,2 +1,3 @@
 export { default as createExpressApp } from "./server/app.js";
 export { default as createRouter } from "./server/router.js";
+export { default as resolve } from './server/resolver.js';
