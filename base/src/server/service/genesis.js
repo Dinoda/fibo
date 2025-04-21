d6e080d9d670be0f7d914ff43998f3ae961db131
Debian <debian@vps-fbb5e062.vps.ovh.net>
2025-04-21 21:25:34 +0000

    Adapted base package for new genesis

--- base/src/server/service/genesis.js
+++ base/src/server/service/genesis.js
@@ -2,10 +2,8 @@ import 'dotenv/config';
 
 import genesis from 'fibo-database-mariadb/genesis';
 
-import files from '../../genesis/load_order.js';
 import database from './database.js';
 
-await genesis(files, {
-  database,
+await genesis(database, {
 });
 process.exit(0);
